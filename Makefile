bbab437de734258048e80201f2719d4c90971214
C-Reaper <gnu.c.reaper@gmail.com>
2025-09-30 17:59:07 +0200

    init commit

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 #CFLAGS = -Wall -Wextra -std=c11 -O2 -g -mavx2
 CFLAGS = -O2 -mavx2 -std=gnu17
 INCLUDES = -Isrc
-LDFLAGS = -lm -lX11 -lpng
+LDFLAGS = -lm
 
 SRC_DIR = src
 BUILD_DIR = build
--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,13 @@
 #include "/home/codeleaded/System/Static/Library/Files.h"
+#include "/home/codeleaded/System/Static/Library/Regex.h"
 
 int main(){
-    
+    Regex rx = Regex_New("\\d\\d");
 
+    Regex_Do(&rx,"01");
+    //Regex_DoFile(&rx,"01");
+
+    Regex_Print(&rx);
+    Regex_Free(&rx);
     return 0;
 }
