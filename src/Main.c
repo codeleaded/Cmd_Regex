d0c2693a1acf49213d59f5ab8ae3e49da4bb8010
C-Reaper <gnu.c.reaper@gmail.com>
2025-10-06 21:18:15 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -2,8 +2,8 @@
 #include "/home/codeleaded/System/Static/Library/Regex.h"
 
 int main(){
-    Regex rx = Regex_Make("a \"@\" a \".\" a");
-    //Regex rx = Regex_Make("[a-z,A-Z]* \"@\" [a-z,A-Z]* \".\" [a-z,A-Z]*");
+    //Regex rx = Regex_Make("a \"@\" a \".\" a");
+    Regex rx = Regex_Make("[a-z,A-Z]* \"@\" [a-z,A-Z]* \".\" [a-z,A-Z]*");
 
     //Regex_Do(&rx,"alexbehringer@kbehringer.de");
     Regex_DoFile(&rx,"./data/File.txt");
