11c9e78308ba1c4df3e681c164bd9c05dfaf466d
codeleaded <codeleaded@gmail.com>
2025-10-11 16:22:01 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -6,10 +6,11 @@ int main(){
     //Regex rx = Regex_Make("[a-z,A-Z]+ \"@\" [a-z,A-Z]+ \".\" [a-z,A-Z]+");
     //Regex rx = Regex_Make("[A-D,P-Z]*E");
     //Regex rx = Regex_Make("[A-D,P-Z]{1,2}E");
-    Regex rx = Regex_Make("[A-D,P-Z]*E");
+    Regex rx = Regex_Make("[c,C][o,O][l,L][o,O]\"u\"?[r,R]");
 
     //Regex_Do(&rx,"AE BBE CZCE ALE PPE WWRT ABCE ABCDE");
-    Regex_DoFile(&rx,"./data/Cartag.txt");
+    Regex_Do(&rx,"Color color coluor colour colorucolouru");
+    //Regex_DoFile(&rx,"./data/Cartag.txt");
 
     Regex_Print(&rx);
     Regex_Free(&rx);
